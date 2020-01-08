21d1ab40ce616949d609911d7a7251c60730a43e
premanuj <mesubedianuj@gmail.com>
2020-01-08 11:54:11 +0545

    admin for news app

--- news/models.py
+++ news/models.py
@@ -5,12 +5,22 @@ from django.contrib.auth.models import User
 # DJANGO-ORM (Object relational mapping)
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+
 class News(models.Model):
-    CATEGORIES = (("0", "Politics"), ("1", "Sports"), ("2", "Business"), ("3", "International"))
     title = models.CharField(max_length=255)
     content = models.TextField()
     count = models.IntegerField(default=0)
-    category = models.CharField(max_length=2, choices=CATEGORIES)
+    slug = models.SlugField(max_length=255, null=True)
+    category = models.ManyToManyField(Category, related_name="news_categoreis")
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
